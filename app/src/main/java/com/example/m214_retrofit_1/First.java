533ad1fe8c8ce77f3a7689fccb5bec39734fb3c6
jamal <ouardijamal87@gmail.com>
2023-02-10 12:24:00 +0100

    Added Fragments & menu

---
+++ app/src/main/java/com/example/m214_retrofit_1/First.java
@@ -0,0 +1,114 @@
+package com.example.m214_retrofit_1;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link First#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class First extends Fragment {
+
+    ListView listView;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment First.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static First newInstance(String param1, String param2) {
+        First fragment = new First();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public First() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_first, container, false);
+
+        listView = v.findViewById(R.id.lv2);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://run.mocky.io/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MovieApi movieApi = retrofit.create(MovieApi.class);
+
+        Call<List<Movie>> call = movieApi.getMovies();
+
+        call.enqueue(new Callback<List<Movie>>() {
+            @Override
+            public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
+                if(response.code()==200)
+                {
+                    List<Movie> movies = response.body();
+
+                    MovieAdapter ma = new MovieAdapter(movies,getActivity(),R.layout.movie_item);
+                    listView.setAdapter(ma);
+                }
+
+
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Movie>> call, Throwable t) {
+                Toast.makeText(getActivity(), "Error contacting API", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
+
+        return v;
+    }
+}
--- app/src/main/java/com/example/m214_retrofit_1/MainActivity.java
+++ app/src/main/java/com/example/m214_retrofit_1/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.m214_retrofit_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -28,39 +32,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        listView = findViewById(R.id.listView);
 
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://run.mocky.io/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        MovieApi movieApi = retrofit.create(MovieApi.class);
-
-        Call<List<Movie>> call = movieApi.getMovies();
-
-        call.enqueue(new Callback<List<Movie>>() {
-            @Override
-            public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
-                if(response.code()==200)
-                {
-                    List<Movie> movies = response.body();
-
-                    MovieAdapter ma = new MovieAdapter(movies,getApplicationContext(),R.layout.movie_item);
-                    listView.setAdapter(ma);
-                }
+        getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, new First()).commit();
 
 
 
-            }
 
-            @Override
-            public void onFailure(Call<List<Movie>> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), "Error contacting API", Toast.LENGTH_SHORT).show();
-            }
-        });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.main_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+
+            case R.id.item_afficher:
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new First()).commit();
+                return true;
+            case R.id.item_ajout:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new Second()).commit();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 }
